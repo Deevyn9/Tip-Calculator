751a1248b7657f985933cad6fa313a7929a46478
Divine Obeten <deevynobeeten@gmail.com>
2021-12-28 15:37:15 +0100

    made tip buttons functional

--- app.js
+++ app.js
@@ -1,23 +1,81 @@
 const billAmount = document.getElementsByClassName('field')[0];
 const customPercentage = document.getElementsByClassName('custom')[0];
 const peopleNumber = document.getElementsByClassName('people-field')[0]; 
-const fivePercent =  document.getElementsByClassName('five')[0];
-const tenPercent = document.getElementsByClassName('ten')[0];
-const fifteenPercent = document.getElementsByClassName('fifteen')[0];
-const twentyFive = document.getElementsByClassName('twenty-five')[0];
-const fiftyPercent = document.getElementsByClassName('fifty')[0];
+const zero = document.getElementsByClassName('zero')[0];
+const tipPercentage = document.querySelectorAll(".tip");
 const tipAmount = document.getElementsByClassName('tip-amount')[0];
 const totalAmount = document.getElementsByClassName('total-amount')[0];
 const resetButton = document.getElementsByClassName('reset-btn')[0];
+let tip = 0, amt = 0, ppl = 0;
 
-peopleNumber.addEventListener("input", (e)=> {
-        updateTotal()
+
+tipPercentage.forEach(btn => {
+    btn.addEventListener("click", ()=>{
+        if(btn.classList.contains("selected")) {
+            tip = 0;
+            btn.classList.remove("selected");
+            btn.classList.add("unselected");
+        } else {
+            tipPercentage.forEach(e => {
+                e.classList.remove("selected");
+            })
+            tip = btn.value;
+            btn.classList.remove("unselected");
+            btn.classList.add("selected");
+        }
+        customPercentage.value = "";
+        amount();
+    });
 });
 
-function updateTotal() {
-    if(peopleNumber.value > 0) {
-        totalAmount.textContent = `$${((billAmount.value)/(peopleNumber.value)).toFixed(2)}`;
+customPercentage.addEventListener("input", ()=>{
+    if(customPercentage.value >= 0) {
+        {    
+        tipPercentage.forEach(e =>{
+            e.classList.remove("selected");
+        })
+            tip = customPercentage.value;
+        amount();
+    }
+    }
+})
+
+billAmount.addEventListener("input", (e) => {
+    amt = Number(billAmount.value)
+    amount()
+});
+
+peopleNumber.addEventListener("input", (e)=> {
+    ppl = peopleNumber.value;
+    if (peopleNumber.value > 0 || peopleNumber.value == "") {
+        amount()
+        zero.style.display = "none";
     } else {
-        totalAmount.textContent = "$0.00"
+        zero.style.display = "flex"
+    }
+});
+
+function amount() {
+    if (amt > 0 && ppl > 0) {
+       let totalTip = (tip*amt)/(100);
+        let totalAmt = amt + totalTip;
+        tipAmount.innerHTML = `$${((totalTip)/(ppl)).toFixed(2)}`;
+        totalAmount.innerHTML = `$${((totalAmt)/(ppl)).toFixed(2)}`;
+    } else {
+        totalAmount.innerHTML = "$0.00"
+        tipAmount.innerHTML= "$0.00"
     }
 }
+
+resetButton.addEventListener('click', ()=> {
+    billAmount.value = "";
+    peopleNumber.value = "";
+    customPercentage.value = "";
+    tipAmount.textContent = "$0.00"
+    totalAmount.textContent = "$0.00"
+
+       tipPercentage.forEach(e =>{
+        e.classList.remove("selected");
+        e.classList.add("unselected");
+    })
+});
